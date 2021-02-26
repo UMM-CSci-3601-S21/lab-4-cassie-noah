929cf99027642c8d7b43f910f3550efe1a7dc895
Cassie Schultz <schu4276@morris.umn.edu>
2021-02-26 17:25:55 -0600

    Add todoController to server
    This adds the TodoController to the server, and also adds the `get` that calls `getTodos()`

--- server/src/main/java/umm3601/Server.java
+++ server/src/main/java/umm3601/Server.java
@@ -10,7 +10,7 @@ import com.mongodb.client.MongoDatabase;
 
 import io.javalin.Javalin;
 import io.javalin.core.util.RouteOverviewPlugin;
-
+import umm3601.todo.TodoController;
 import umm3601.user.UserController;
 
 public class Server {
@@ -36,6 +36,7 @@ public class Server {
 
     // Initialize dependencies
     UserController userController = new UserController(database);
+    TodoController todoController = new TodoController(database);
 
     Javalin server = Javalin.create(config -> {
       config.registerPlugin(new RouteOverviewPlugin("/api"));
@@ -59,6 +60,8 @@ public class Server {
 
     // List users, filtered using query parameters
     server.get("/api/users", userController::getUsers);
+    //List todos, filtered using query parameters
+    server.get("api/todos", todoController::getTodos);
 
     // Get the specified user
     server.get("/api/users/:id", userController::getUser);
@@ -69,6 +72,7 @@ public class Server {
     // Add new user with the user info being in the JSON body
     // of the HTTP request
     server.post("/api/users", userController::addNewUser);
+
 
     server.exception(Exception.class, (e, ctx) -> {
       ctx.status(500);

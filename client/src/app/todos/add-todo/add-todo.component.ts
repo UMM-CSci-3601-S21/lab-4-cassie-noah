5d49b871cbe514616c2e9fe41e4441de6a9adb2d
Noah <const112@umn.edu>
2021-02-28 15:12:49 -0600

    Add Todo Validation Message Property
    Added addTodoValidationMessage property and defines the requirements for the added todos.

--- client/src/app/todos/add-todo/add-todo.component.ts
+++ client/src/app/todos/add-todo/add-todo.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { Todo } from '../todo';
+import { TodoService } from '../todo.service';
 
 @Component({
   selector: 'app-add-todo',
@@ -6,6 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-todo.component.scss']
 })
 export class AddTodoComponent implements OnInit {
+
+  addTodoForm: FormGroup;
+
+  todo: Todo;
+
+  addTodoValidationMessages = {
+    owner: [
+      {type: 'required', message: 'Owner is required'},
+      {type: 'minlength', message: 'Owner must be at least 2 characters long'},
+      {type: 'maxlength', message: 'Owner cannot be more than 50 characters long'},
+    ],
+
+    category: [
+      {type: 'required', message: 'Category is required'},
+      {type: 'minlength', message: 'Category must be at least 2 characters long'},
+      {type: 'maxlength', message: 'Category cannot be more than 50 characters long'},
+    ],
+
+    status: [
+      {type: 'required', message: 'Status must be true/false'},
+      {type: 'required', message: 'Status is required'}
+    ],
+
+    body: [
+      { type: 'required', message: 'Body is required' },
+      {type: 'minlength', message: 'Body must be at least 2 characters long'},
+    ]
+  };
 
   constructor() { }
 

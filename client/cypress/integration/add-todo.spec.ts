29afb5e6c30129c1a6901480e738b3083bffe634
Cassie Schultz <schu4276@morris.umn.edu>
2021-03-02 13:10:06 -0600

    Add page object and spec file for add todos e2e
    This adds the two files we need for e2e testing of the add todos component

---
+++ client/cypress/integration/add-todo.spec.ts
@@ -0,0 +1,2 @@
+import {Todo } from 'src/app/todos/todo';
+import {AddTodoPage} from '../support/add-todo.po';
---
+++ client/cypress/support/add-todo.po.ts
@@ -0,0 +1,8 @@
+export class AddTodoPage{
+  navigateTo(){
+    return cy.visit('/todos/new');
+  }
+
+
+
+}
